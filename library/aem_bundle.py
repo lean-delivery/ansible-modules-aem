2bbfe26651f584f3fbd9a7b592ffa3b8e4e74d08
MaksimShchuplov <shchuplov@gmail.com>
2019-02-20 11:05:27 +0000

    fixing pep8 , tests

--- library/aem_bundle.py
+++ library/aem_bundle.py
@@ -2,8 +2,10 @@
 # -*- coding: utf-8 -*-
 
 # Copyright: (c) 2019, Lean Delivery Team <team@lean-delivery.com>
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+# GNU General Public License v3.0+ (see COPYING or
+# https://www.gnu.org/licenses/gpl-3.0.txt)
 
+import requests
 ANSIBLE_METADATA = {'metadata_version': '1.1',
                     'status': ['preview'],
                     'supported_by': 'community'}
@@ -67,27 +69,27 @@ EXAMPLES = u'''
     url: https://aem-node.example.com:4502
 '''
 
-import requests
 
 class AEMBundle(object):
     """docstring for AEMBundle"""
+
     def __init__(self, arg):
         super(AEMBundle, self).__init__()
-        self.arg            = arg
-        self.name           = self.module.params['name']
-        self.action          = self.module.params['action']
-        self.admin_user     = self.module.params['admin_user']
+        self.arg = arg
+        self.name = self.module.params['name']
+        self.action = self.module.params['action']
+        self.admin_user = self.module.params['admin_user']
         self.admin_password = self.module.params['admin_password']
-        self.url            = self.module.params['url']
-        self.changed        = False
-        self.msg            = []
+        self.url = self.module.params['url']
+        self.changed = False
+        self.msg = []
         self._get_bnd_status()
 
     def _get_bnd_status(self):
-        aem_request = requests.get('%s/system/console/bundles/%s.json' % 
-                                   (self.url, self.name), 
-                                    auth=(self.admin_user, 
-                                    self.admin_password))
+        aem_request = requests.get('%s/system/console/bundles/%s.json' %
+                                   (self.url, self.name),
+                                   auth=(self.admin_user,
+                                         self.admin_password))
         if aem_request.status_code == 200:
             self.exists = True
             if aem_request.json['data'][0]['state'] == 'Active':
@@ -97,16 +99,21 @@ class AEMBundle(object):
         else:
             self.exists = False
             self.active = False
-    
+
     def do_action(self):
-        aem_request = requests.post('%s/system/console/bundles/%s' % (self.url, self.name),
-                                    data={'action': self.action },
-                                    auth=(self.admin_user, self.admin_password))
+        aem_request = requests.post(
+            '%s/system/console/bundles/%s' %
+            (self.url, self.name), data={
+                'action': self.action}, auth=(
+                self.admin_user, self.admin_password))
         if aem_request.status_code != 200:
-            self.module.fail_json(msg='failed to perform %s action on %s bundle - %s' % 
-                                  (self.action, aem_request.status_code, aem_request.json))
+            self.module.fail_json(
+                msg='failed to perform %s action on %s bundle - %s' %
+                (self.action, aem_request.status_code, aem_request.json))
         self.changed = True
-        self.msg.append('action %s was performmed on bundle %s' % (self.action, self.name))
+        self.msg.append(
+            'action %s was performmed on bundle %s' %
+            (self.action, self.name))
 
     def show_message(self):
         if self.changed:
@@ -115,11 +122,18 @@ class AEMBundle(object):
         else:
             self.module.exit_json(changed=False)
 
+
 def main():
     module = AnsibleModule(
         argument_spec=dict(
             name=dict(required=True, type='str'),
-            action=dict(default='start', type='str', choices=['start', 'stop', 'refresh']),
+            action=dict(
+                default='start',
+                type='str',
+                choices=[
+                    'start',
+                    'stop',
+                    'refresh']),
             admin_user=dict(required=True, type='str'),
             admin_password=dict(required=True, type='str', no_log=True),
             url=dict(required=True, type='str')
@@ -131,5 +145,6 @@ def main():
     bundle.do_action()
     bundle.show_message()
 
-#<<INCLUDE_ANSIBLE_MODULE_COMMON>>
+
+# <<INCLUDE_ANSIBLE_MODULE_COMMON>>
 main()
