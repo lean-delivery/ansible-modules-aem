d6dd2b8e8c8e9e59f653e6d451df8cb999a3c2f6
Maksim Shchuplov <shchuplov@gmail.com>
2019-03-22 17:45:52 +0300

    Aem password (#5)
    * init
    
    * removed version
    
    * fixed old/new passwords
    
    * fixes
    
    * fixes

--- library/aem_password.py
+++ library/aem_password.py
@@ -10,23 +10,16 @@
 
 # Note: you must supply both the old and new passwords for this to work
 
-import sys
-import os
-import platform
-import httplib
-import urllib
-import base64
-import json
-import string
-import random
+
+import requests
 
 DOCUMENTATION = '''
 ---
-module: aempassword
+module: aem_password
 short_description: Change Adobe CQ Password
 description:
     - Change Adobe CQ Password
-author: Paul Markham
+author: Paul Markham, Lean Delivery Team
 notes:
     - This is mostly used to change the Admin password from the default of 'admin'.
 options:
@@ -54,45 +47,38 @@ options:
         description:
             - Return ok if neither the old nor new passwords are valid for the user.
         required: false
-    version:
-        description:
-            - version of AEM this module is running against..
-        required: false
-        default: 6.0.0
 '''
 
-EXAMPLES='''
+EXAMPLES = '''
 # Change admin password from default
-- aempassword: id=admin
-              old_password=admin
-              new_password=S3cr3t
-              host=auth01
-              port=4502
+- aem_password: 
+    id: admin
+    old_password: admin
+    new_password: S3cr3t
+    host: "http://localhost"
+    port: 4502
 '''
+
 
 # --------------------------------------------------------------------------------
 # AEMPassword class.
 # --------------------------------------------------------------------------------
 class AEMPassword(object):
     def __init__(self, module):
-        self.module            = module
-        self.id                = self.module.params['id']
-        self.new_password      = self.module.params['new_password']
+        self.module = module
+        self.id = self.module.params['id']
+        self.new_password = self.module.params['new_password']
         self.old_password_list = self.module.params['old_password']
-        self.ignore_err        = self.module.params['ignore_err']
-        self.host              = self.module.params['host']
-        self.port              = self.module.params['port']
-        self.version           = self.module.params['version']
-        
+        self.ignore_err = self.module.params['ignore_err']
+        self.host = str(self.module.params['host'])
+        self.port = str(self.module.params['port'])
+        self.url = self.host + ':' + self.port
+
         self.changed = False
         self.msg = []
         self.id_initial = self.id[0]
 
-        ver = self.version.split('.')
-        if int(ver[0]) >= 6 and int(ver[1]) >= 1:
-            self.aem61 = True
-        else:
-            self.aem61 = False
+        self.aem61 = True
 
         self.get_user_info()
 
@@ -104,10 +90,13 @@ class AEMPassword(object):
         # check if new password is already valid
         self.msg.append('checking new password')
         if self.aem61:
-            (status, output) = self.http_request('GET', '/bin/querybuilder.json?path=/home/users&1_property=rep:authorizableId&1_property.value=%s&p.limit=-1' % (self.id), user = self.id, password = self.new_password)
+            r = requests.get(self.url + '/bin/querybuilder.json?path=/home/users&'
+                                        '1_property=rep:authorizableId&1_property.value=%s&p.limit=-1'
+                             % self.id, auth=(self.id, self.new_password))
         else:
-            (status, output) = self.http_request('GET', '/home/users/%s/%s.rw.json?props=*' % (self.id_initial, self.id), user = self.id, password = self.new_password)
-        if status == 200:
+            r = requests.get(self.url + '/home/users/%s/%s.rw.json?props=*'
+                             % (self.id_initial, self.id), auth=(self.id, self.new_password))
+        if r.status_code == 200:
             self.msg.append("password doesn't need to be changed")
             self.exit_status()
 
@@ -116,10 +105,13 @@ class AEMPassword(object):
         for password in self.old_password_list:
             self.msg.append('checking password "%s"' % password)
             if self.aem61:
-                (status, output) = self.http_request('GET', '/bin/querybuilder.json?path=/home/users&1_property=rep:authorizableId&1_property.value=%s&p.limit=-1' % (self.id), user = self.id, password = password)
+                r = requests.get(self.url + '/bin/querybuilder.json?path=/home/users&1_property=rep:authorizableId&'
+                                            '1_property.value=%s&p.limit=-1'
+                                 % self.id, auth=(self.id, password))
             else:
-                (status, output) = self.http_request('GET', '/home/users/%s/%s.rw.json?props=*' % (self.id_initial, self.id), user = self.id, password = password)
-            if status == 200:
+                r = requests.get(self.url + '/home/users/%s/%s.rw.json?props=*'
+                                 % (self.id_initial, self.id), auth=(self.id, password))
+            if r.status_code == 200:
                 old_password_valid = True
                 self.old_password = password
                 break
@@ -140,38 +132,20 @@ class AEMPassword(object):
                     ('plain', self.new_password),
                     ('verify', self.new_password),
                     ('old', self.old_password),
-                    ]
-                (status, output) = self.http_request('POST', '/crx/explorer/ui/setpassword.jsp', user = self.id, password = self.old_password, fields = fields)
+                ]
+                r = requests.post(self.url + '/crx/explorer/ui/setpassword.jsp',
+                                  auth=(self.id, self.old_password), data=fields)
             else:
                 fields = [
                     (':currentPassword', self.old_password),
                     ('rep:password', self.new_password),
-                    ]
-                (status, output) = self.http_request('POST', '/home/users/%s/%s.rw.html' % (self.id_initial, self.id), user = self.id, password = self.old_password, fields = fields)
-##            self.exit_status()
-            if status != 200:
-                self.module.fail_json(msg='failed to change password: %s - %s' % (status, output))
+                ]
+                r = requests.post(self.url + '/home/users/%s/%s.rw.html'
+                                  % (self.id_initial, self.id), auth=(self.id, self.old_password), data=fields)
+            if r.status_code != 200:
+                self.module.fail_json(msg='failed to change password: %s - %s' % (r.status_code, r.text))
         self.changed = True
         self.msg.append('password changed')
-
-    # --------------------------------------------------------------------------------
-    # Issue http request.
-    # --------------------------------------------------------------------------------
-    def http_request(self, method, url, user, password, fields = None):
-        headers = {'Authorization' : 'Basic ' + base64.b64encode(str(user) + ':' + str(password))}
-        if fields:
-            data = urllib.urlencode(fields)
-            headers['Content-type'] = 'application/x-www-form-urlencoded'
-        else:
-            data = None
-        conn = httplib.HTTPConnection(self.host + ':' + str(self.port))
-        try:
-            conn.request(method, url, data, headers)
-        except Exception as e:
-            self.module.fail_json(msg="http request '%s %s' failed: %s" % (method, url, e))
-        resp = conn.getresponse()
-        output = resp.read()
-        return (resp.status, output)
 
     # --------------------------------------------------------------------------------
     # Return status and msg to Ansible.
@@ -180,30 +154,31 @@ class AEMPassword(object):
         msg = ','.join(self.msg)
         self.module.exit_json(changed=self.changed, msg=msg)
 
+
 # --------------------------------------------------------------------------------
 # Mainline.
 # --------------------------------------------------------------------------------
 def main():
     module = AnsibleModule(
-        argument_spec      = dict(
-            id             = dict(required=True),
-            new_password   = dict(required=True, no_log=True),
-            old_password   = dict(required=True, type='list', no_log=True),
-            admin_user     = dict(required=False), # no longer needed
-            admin_password = dict(required=False, no_log=True), # no longer needed
-            host           = dict(required=True),
-            port           = dict(required=True, type='int'),
-            ignore_err     = dict(default=False, type='bool'),
-            version        = dict(default='6.0.0'),
-            ),
+        argument_spec=dict(
+            id=dict(required=True),
+            new_password=dict(required=True, no_log=True),
+            old_password=dict(required=True, type='list', no_log=True),
+            admin_user=dict(required=False),  # no longer needed
+            admin_password=dict(required=False, no_log=True),  # no longer needed
+            host=dict(required=True),
+            port=dict(required=True, type='int'),
+            ignore_err=dict(default=False, type='bool'),
+        ),
         supports_check_mode=True
-        )
+    )
 
     password = AEMPassword(module)
-    
+
     password.set_password()
 
     password.exit_status()
+
 
 # --------------------------------------------------------------------------------
 # Ansible boiler plate code.
