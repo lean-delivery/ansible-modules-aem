f979b893787b4e4ad065b13d8253f8aba0351065
Maksim Shchuplov <shchuplov@gmail.com>
2019-03-22 17:46:12 +0300

    Aem group (#4)
    * init
    
    * create/delete group
    
    * add permissions to group
    
    * fixes and cleanup
    
    * add to root group
    
    * fixed path
    
    * fixed groups
    
    * remove changed in add_to_root_group
    
    * added changed check to root_group
    
    * fixed groups management
    
    * added todo
    
    * added mult root groups
    
    * fixed readme

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 AEM Ansible modules and roles.
 
 Under impression of https://github.com/pmarkham/ansible-adobe-aem.git
+
+TODO:
+Parent groups check.
--- library/aem_group.py
+++ library/aem_group.py
@@ -6,25 +6,18 @@
 # GNU General Public License v3.0+ (see COPYING or
 # https://www.gnu.org/licenses/gpl-3.0.txt)
 
-import sys
-import os
-import platform
-import httplib
-import urllib
-import base64
-import json
+import requests
 
 DOCUMENTATION = '''
 ---
-module: aemgroup
+module: aem_group
 short_description: Manage AEM groups
 description:
-    - Create, modify and delete AEM groups
+    - Create, modify, delete and manage permissions AEM groups
 author: Paul Markham
+contributors: Lean Delivery Team
 notes:  
-        - This module only does basic group management. It doesn't handle access control lists so it's not that useful.
-          It's more to create groups that contain other groups, e.g. a 'sysadmin' group is in the administrator group;
-          'developers' are in the 'readonly' group.
+        - This module does group management. 
 options:
     id:
         description:
@@ -46,6 +39,16 @@ options:
               Only required when creating a new account.
         required: true
         default: null
+    root_groups:
+        description:
+            - List of parent group.
+        required: False
+        default: null    
+    permissions:
+        description:
+            - Set of permissions for group.
+        required: False
+        default: null    
     admin_user:
         description:
             - AEM admin user account name
@@ -64,43 +67,57 @@ options:
         required: true
 '''
 
-EXAMPLES='''
+EXAMPLES = '''
 # Create a group
-- aemgroup: id=sysadmin
-           name='Systems Administrators'
-           groups='administrators'
-           host=auth01
-           port=4502
-           admin_user=admin
-           admin_password=admin
-           state=present
+- aem_group: 
+    id: sysadmin
+    name: 'Systems Administrators'
+    groups: 'administrators'
+    host: 'http://example.com'
+    port: 4502
+    admin_user: admin
+    admin_password: admin
+    root_groups:
+        - everyone
+    permissions:
+        - 'path:/,read:true'
+        - 'path:/etc/packages,read:true,modify:true,create:true,delete:false,replicate:true'    
+    state: present
 
 # Delete a group
-- aemgroup: id=devs
-           host=auth01
-           port=4502
-           admin_user=admin
-           admin_password=admin
-           state=absent
+- aem_group: 
+    id: devs
+    host: 'http://example.com'
+    port: 4502
+    admin_user: admin
+    admin_password: admin
+    state: absent
 '''
+
 
 # --------------------------------------------------------------------------------
 # AEMGroup class.
 # --------------------------------------------------------------------------------
+
+
 class AEMGroup(object):
     def __init__(self, module):
-        self.module         = module
-        self.state          = self.module.params['state']
-        self.id             = self.module.params['id']
-        self.name           = self.module.params['name']
-        self.groups         = self.module.params['groups']
-        self.admin_user     = self.module.params['admin_user']
-        self.admin_password = self.module.params['admin_password']
-        self.host           = self.module.params['host']
-        self.port           = self.module.params['port']
-        self.version        = self.module.params['version']
+        self.module = module
+        self.state = str(self.module.params['state'])
+        self.id = str(self.module.params['id'])
+        self.name = str(self.module.params['name'])
+        self.groups = self.module.params['groups']
+        self.admin_user = str(self.module.params['admin_user'])
+        self.admin_password = str(self.module.params['admin_password'])
+        self.host = str(self.module.params['host'])
+        self.port = str(self.module.params['port'])
+        self.url = str(self.host + ':' + self.port)
+        self.auth = (self.admin_user, self.admin_password)
+        self.permissions = self.module.params['permissions']
+        self.root_groups = self.module.params['root_groups']
+        self.exists = False
+        self.root_groups_path = []
 
-        
         self.changed = False
         self.msg = []
         self.id_initial = self.id[0]
@@ -108,29 +125,22 @@ class AEMGroup(object):
         if self.module.check_mode:
             self.msg.append('Running in check mode')
 
-        self.aem61 = False
-        ver = self.version.split('.')
-        if int(ver[0]) >= 6:
-            # aem6 must have all groups and all users in the 'everyone' group 
-            if not "everyone" in self.groups:
-                 # everyone group not listed, so add it
-                 self.groups.append("everyone")
-            if int(ver[1]) >=1:
-                self.aem61 = True
-
+        self.aem61 = True
         self.get_group_info()
 
-
-
     # --------------------------------------------------------------------------------
-    # Look up package info.
+    # Look up group info.
     # --------------------------------------------------------------------------------
     def get_group_info(self):
         if self.aem61:
-            (status, output) = self.http_request('GET', '/bin/querybuilder.json?path=/home/groups&1_property=rep:authorizableId&1_property.value=%s&p.limit=-1&p.hits=full' % self.id)
-            if status != 200:
-                self.module.fail_json(msg='Error searching for group. status=%s output=%s' % (status, output))
-            info = json.loads(output)
+            r = requests.get(
+                self.url + '/bin/querybuilder.json?path=/home/groups&1_property=rep'
+                           ':authorizableId&1_property.value=%s&p.limit=-1&p.hits=full' % self.id,
+                auth=self.auth
+            )
+            if r.status_code != 200:
+                self.module.fail_json(msg='Error searching for group. status=%s output=%s' % (r.status_code, r.text))
+            info = r.json()
             if len(info['hits']) == 0:
                 self.exists = False
                 return
@@ -138,16 +148,40 @@ class AEMGroup(object):
         else:
             self.path = '/home/groups/%s/%s' % (self.id_initial, self.id)
 
-        (status, output) = self.http_request('GET', '%s.rw.json?props=*' % (self.path))
-        if status == 200:
+        r = requests.get(self.url + '%s.rw.json?props=*' % (self.path), auth=self.auth)
+        if r.status_code == 200:
             self.exists = True
-            info = json.loads(output)
+            info = r.json()
             self.curr_name = info['name']
             self.curr_groups = []
-            for entry in info['declaredMemberOf']:
+            self.curr_root_groups = []
+            for group in info["memberOf"]:
+                self.curr_root_groups.append(group["name"])
+            for entry in info['declaredMembers']:
                 self.curr_groups.append(entry['authorizableId'])
         else:
             self.exists = False
+
+    # --------------------------------------------------------------------------------
+    # Look up root group info.
+    # --------------------------------------------------------------------------------
+    def get_root_groups_path(self):
+        if self.aem61:
+            for root_group in self.root_groups:
+                r = requests.get(
+                    self.url + '/bin/querybuilder.json?path=/home/groups&1_property=rep'
+                               ':authorizableId&1_property.value=%s&p.limit=-1&p.hits=full' % root_group,
+                    auth=self.auth
+                )
+                if r.status_code != 200:
+                    self.module.fail_json(
+                        msg='Error searching for root group. status=%s output=%s' % (r.status_code, r.text))
+                info = r.json()
+                if len(info['hits']) == 0:
+                    self.exists = False
+                    return
+                for hit in info['hits']:
+                    self.root_groups_path.append(hit['jcr:path'])
 
 
     # --------------------------------------------------------------------------------
@@ -159,7 +193,6 @@ class AEMGroup(object):
             if self.name:
                 if self.curr_name != self.name:
                     self.update_name()
-
             if self.groups:
                 self.curr_groups.sort()
                 self.groups.sort()
@@ -167,11 +200,19 @@ class AEMGroup(object):
                 groups = ','.join(self.groups).lower()
                 if curr_groups != groups:
                     self.update_groups()
+            self.add_permissions()
+            if self.root_groups:
+                self.get_root_groups_path()
+                self.add_to_root_groups()
         else:
             # Create new group
             if not self.name:
                 self.module.fail_json(msg='Missing required argument: name')
             self.create_group()
+            self.add_permissions()
+            if self.root_groups:
+                self.get_root_groups_path()
+                self.add_to_root_groups()
 
     # --------------------------------------------------------------------------------
     # state='absent'
@@ -187,28 +228,25 @@ class AEMGroup(object):
         fields = [
             ('createGroup', ''),
             ('authorizableId', self.id),
-            ('profile/givenName', self.name),
-            ]
+            ('./profile/givenName', self.name),
+        ]
         if not self.module.check_mode:
-            if self.groups:
-                for group in self.groups:
-                    fields.append(('membership', group))
-            (status, output) = self.http_request('POST', '/libs/granite/security/post/authorizables', fields)
+            r = requests.post(self.url + '/libs/granite/security/post/authorizables', auth=self.auth, data=fields)
             self.get_group_info()
-            if status != 201 or not self.exists:
-                self.module.fail_json(msg='failed to create group: %s - %s' % (status, output))
+            if r.status_code != 201 or not self.exists:
+                self.module.fail_json(msg='failed to create group: %s - %s' % (r.status_code, r.text))
         self.changed = True
-        self.msg.append("group '%s' created" % (self.id))
-    
+        self.msg.append("group '%s' created" % self.id)
+
     # --------------------------------------------------------------------------------
     # Update name
     # --------------------------------------------------------------------------------
     def update_name(self):
         fields = [('profile/givenName', self.name)]
         if not self.module.check_mode:
-            (status, output) = self.http_request('POST', '%s.rw.html' % (self.path), fields)
-            if status != 200:
-                self.module.fail_json(msg='failed to update name: %s - %s' % (status, output))
+            r = requests.post(self.url + '%s/.rw.html' % self.path, auth=self.auth, data=fields)
+            if r.status_code != 200:
+                self.module.fail_json(msg='failed to update name: %s - %s' % (r.status_code, r.text))
         self.changed = True
         self.msg.append("name changed from '%s' to '%s'" % (self.curr_name, self.name))
 
@@ -216,15 +254,29 @@ class AEMGroup(object):
     # Update groups
     # --------------------------------------------------------------------------------
     def update_groups(self):
-        fields = []
-        if not self.module.check_mode:
+        fields = {"memberAction": "addMembers"}
+        if not self.module.check_mode and self.groups:
             for group in self.groups:
-                fields.append(('membership', group))
-            (status, output) = self.http_request('POST', '%s.rw.html' % (self.path), fields)
-            if status != 200:
-                self.module.fail_json(msg='failed to update groups: %s - %s' % (status, output))
+                fields['memberEntry'] = group
+                r = requests.post(self.url + '%s' % self.path, auth=self.auth, files={"memberAction": "addMembers", "memberEntry": "administrators"})
+            if r.status_code != 200:
+                self.module.fail_json(msg='failed to update groups: %s - %s' % (r.status_code, r.text))
         self.changed = True
         self.msg.append("groups updated from '%s' to '%s'" % (self.curr_groups, self.groups))
+
+    # --------------------------------------------------------------------------------
+    # Add to root group
+    # --------------------------------------------------------------------------------
+    def add_to_root_groups(self):
+        if not self.module.check_mode:
+            for root_group_path in self.root_groups_path:
+                fields = [('addMembers', self.id)]
+                r = requests.post(self.url + '%s/.rw.html' % root_group_path, auth=self.auth, data=fields)
+                if r.status_code != 200:
+                    self.module.fail_json(msg='failed to add to root group: %s - %s' % (r.status_code, r.text))
+                self.msg.append("group added to '%s'" % root_group_path)
+            #if len(set(self.curr_root_groups).symmetric_difference(self.root_groups)) > 0:
+            #    self.changed = True
 
     # --------------------------------------------------------------------------------
     # Delete a group
@@ -232,30 +284,26 @@ class AEMGroup(object):
     def delete_group(self):
         fields = [('deleteAuthorizable', '')]
         if not self.module.check_mode:
-            (status, output) = self.http_request('POST', '%s.rw.html' % (self.path), fields)
-            if status != 200:
-                self.module.fail_json(msg='failed to delete group: %s - %s' % (status, output))
+            r = requests.post(self.url + '%s/.rw.html' % self.path, auth=self.auth, data=fields)
+            if r.status_code != 200:
+                self.module.fail_json(msg='failed to delete group: %s - %s' % (r.status_code, r.text))
         self.changed = True
-        self.msg.append("group '%s' deleted" % (self.id))
+        self.msg.append("group '%s' deleted" % self.id)
 
     # --------------------------------------------------------------------------------
-    # Issue http request.
+    # Add permissions to a group
     # --------------------------------------------------------------------------------
-    def http_request(self, method, url, fields = None):
-        headers = {'Authorization' : 'Basic ' + base64.b64encode(self.admin_user + ':' + self.admin_password)}
-        if fields:
-            data = urllib.urlencode(fields)
-            headers['Content-type'] = 'application/x-www-form-urlencoded'
-        else:
-            data = None
-        conn = httplib.HTTPConnection(self.host + ':' + str(self.port))
-        try:
-            conn.request(method, url, data, headers)
-        except Exception as e:
-            self.module.fail_json(msg="http request '%s %s' failed: %s" % (method, url, e))
-        resp = conn.getresponse()
-        output = resp.read()
-        return (resp.status, output)
+    def add_permissions(self):
+        for permission in self.permissions:
+            fields = [
+                ('authorizableId', self.id),
+                ('_charset_', 'utf - 8'),
+                ('changelog', permission),
+            ]
+            if not self.module.check_mode:
+                r = requests.post(self.url + '/.cqactions.html', auth=self.auth, data=fields)
+                if r.status_code != 200 or not self.exists:
+                    self.module.fail_json(msg='failed to add permissions to a group')
 
     # --------------------------------------------------------------------------------
     # Return status and msg to Ansible.
@@ -270,24 +318,24 @@ class AEMGroup(object):
 # --------------------------------------------------------------------------------
 def main():
     module = AnsibleModule(
-        argument_spec      = dict(
-            id             = dict(required=True),
-            state          = dict(required=True, choices=['present', 'absent']),
-            name           = dict(default=None),
-            groups         = dict(default=None, type='list'),
-            admin_user     = dict(required=True),
-            admin_password = dict(required=True, no_log=True),
-            host           = dict(required=True),
-            port           = dict(required=True, type='int'),
-            version        = dict(required=True),
-            ),
+        argument_spec=dict(
+            id=dict(required=True),
+            state=dict(required=True, choices=['present', 'absent']),
+            name=dict(default=None),
+            groups=dict(default=None, type='list'),
+            admin_user=dict(required=True),
+            admin_password=dict(required=True, no_log=True),
+            host=dict(required=True),
+            port=dict(required=True, type='int'),
+            root_groups=dict(required=False, type='list'),
+            permissions=dict(default=None, type='list'),
+        ),
         supports_check_mode=True
-        )
+    )
 
     group = AEMGroup(module)
-    
-    state = module.params['state']
 
+    state = module.params['state']
 
     if state == 'present':
         group.present()
@@ -297,6 +345,7 @@ def main():
         module.fail_json(msg='Invalid state: %s' % state)
 
     group.exit_status()
+
 
 # --------------------------------------------------------------------------------
 # Ansible boiler plate code.
